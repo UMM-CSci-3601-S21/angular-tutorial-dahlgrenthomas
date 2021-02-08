7592aa8e7c718776dcf9ce8ba48eaa82fc0fbdd7
Thomas <dahlg136@morris.umn.edu>
2021-02-07 21:31:41 -0600

    Cart + Shipping
    Added the ability to add items to the cart and also Made it so you can view the cost of shipping on those items. Also added a fix for viewing shipping prices in the actual web page on github.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,8 +14,12 @@ export class ProductDetailsComponent implements OnInit {
   product;
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
-
+  addToCart(product): void {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
   ngOnInit(): void {
     // First get the product id from the current route.
   const routeParams = this.route.snapshot.paramMap;
